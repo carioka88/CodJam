95659dc071c980d993b2cfe3cfd187b89bd9c456
carioka88 <rociorama88@gmail.com>
2018-02-12 11:40:56 +0100

    Add standard I/O

--- sheeps.py
+++ sheeps.py
@@ -1,19 +1,21 @@
-
-number = 1692
-check_number = [0] * 10
-
 def check_digits(number, array):
   while number > 0:
     index = number%10
     array[index] = 1
     number = number//10
 
-count = 1
-while (0 in check_number and count < 100):
-  check_digits(number*count, check_number)
-  count = count + 1
+t = int(raw_input())  # read a line with a single integer
+for i in xrange(1, t + 1):
+  number = int(raw_input())  # read a int
+  check_number = [0] * 10
+  count = 1
+  if number != 0:
+    while (0 in check_number and count < 100):
+      check_digits(number*count, check_number)
+      count = count + 1
 
-if 0 in check_number:
-  print "INSOMNIA"
-else:
-  print number*(count - 1)
+  if 0 in check_number:
+    print "Case #{}: {}".format(i, "INSOMNIA")
+  else:
+    print "Case #{}: {}".format(i, number*(count-1))
+
